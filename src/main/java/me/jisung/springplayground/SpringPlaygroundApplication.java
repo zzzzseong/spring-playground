60da80e062e87f21df5735e7b5a4543cdbe5780d
zzzzseong <jung48182@gmail.com>
2024-09-22 16:30:47 +0900

    initial commit

---
+++ src/main/java/me/jisung/springplayground/SpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package me.jisung.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jisung/springplayground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jisung.springplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
