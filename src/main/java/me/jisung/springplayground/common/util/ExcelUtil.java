2c32aa3257fe97ac7c2385926925603a56628d4a
zzzzseong <jung48182@gmail.com>
2024-10-02 10:42:32 +0900

    refactor(excel-util): 엑셀 다운로드 함수 리팩터링

--- src/main/java/me/jisung/springplayground/common/util/ExcelUtil.java
+++ src/main/java/me/jisung/springplayground/common/util/ExcelUtil.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+
+import me.jisung.springplayground.common.exception.ApiErrorCode;
+import me.jisung.springplayground.common.exception.ApiException;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -21,11 +24,16 @@ import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.HttpStatus;
 
 public class ExcelUtil {
 
     private static final int MAX_ROW_COUNT = 1_000_000;
     private static final int MIN_ROW_COUNT = 1;
+
+    private static final Color HEADER_COLOR = new Color(20, 171, 177);
+    private static final short HEADER_HEIGHT = 500;
+    private static final int HEADER_WIDTH = 5000;
 
     private ExcelUtil() {
         throw new IllegalStateException("Utility class cannot be instantiated");
@@ -35,35 +43,40 @@ public class ExcelUtil {
      * 리스트에 담긴 데이터를 POI Workbook 객체로 변환해 반환하는 메서드
      * @param list  변환 대상 리스트 - 제네릭 클래스에 @ExcelColumn 어노테이션 필요
      * @param clazz 리스트 제네릭 클래스 객체
+     * @param response HttpServletResponse 객체
+     * @param filename 다운로드 받을 파일 이름
+     * @param sheetName 엑셀 시트 이름
      */
     public static void listToExcel(List<?> list, Class<?> clazz, HttpServletResponse response, String filename, String sheetName) {
-        if(list.size() <= MIN_ROW_COUNT || list.size() >= MAX_ROW_COUNT) {
-            throw new IllegalArgumentException("MAX_ROW_COUNT: " + MAX_ROW_COUNT + ", MIN_ROW_COUNT: " + MIN_ROW_COUNT);
-        }
+        if(list.size() <= MIN_ROW_COUNT) throw new ApiException(HttpStatus.BAD_REQUEST, "row count is too small - min row cnt: " + MIN_ROW_COUNT);
+        if(list.size() >= MAX_ROW_COUNT) throw new ApiException(HttpStatus.BAD_REQUEST, "row count is too long - max row cnt: " + MAX_ROW_COUNT);
 
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet(sheetName);
 
         Row header = sheet.createRow(0);
-        header.setHeight((short) 500);
+        header.setHeight(HEADER_HEIGHT);
 
         List<Field> fieldList = getFieldList(clazz);
 
-        // ADD HEADER ROW TO EXCEL
+        // add header row
         XSSFCellStyle headerStyle = (XSSFCellStyle) workbook.createCellStyle();
-        setHeaderCellStyle(headerStyle, new XSSFColor(new Color(20, 171, 177), new DefaultIndexedColorMap()));
+        setHeaderCellStyle(headerStyle, new XSSFColor(HEADER_COLOR, new DefaultIndexedColorMap()));
         for (int i = 0; i < fieldList.size(); i++) {
             Cell headerCell = header.createCell(i);
+
             headerCell.setCellValue(fieldList.get(i).getAnnotation(ExcelColumn.class).headerName());
             headerCell.setCellStyle(headerStyle);
-            sheet.setColumnWidth(i, 5000);
+
+            sheet.setColumnWidth(i, HEADER_WIDTH);
         }
 
-        // ADD BODY ROW TO EXCEL
+        // add body rows
         try {
             int rowCnt = 1;
             XSSFCellStyle bodyStyle = (XSSFCellStyle) workbook.createCellStyle();
             setBodyCellStyle(bodyStyle);
+
             for (int i = 0; i < list.size(); i++) {
                 Row row = sheet.createRow(rowCnt++);
                 Object element = list.get(i);
@@ -71,12 +84,9 @@ public class ExcelUtil {
                 for (int j = 0; j < fieldList.size(); j++) {
                     Cell bodyCell = row.createCell(j);
                     bodyCell.setCellStyle(bodyStyle);
-                    Field field = fieldList.get(j);
-                    field.setAccessible(true);
-                    Object value = field.get(element);
+                    Object value = fieldList.get(j).get(element);
 
-                    if (value instanceof Number) {
-                        Number numberValue = (Number) value;
+                    if (value instanceof Number numberValue) {
                         bodyCell.setCellValue(numberValue.doubleValue());
                         continue;
                     }
@@ -85,12 +95,11 @@ public class ExcelUtil {
                 }
             }
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            throw new ApiException(e, ApiErrorCode.UNAUTHORIZED);
         }
 
         response.setContentType("ms-vnd/excel");
         response.setHeader("Content-Disposition", "attachment;filename=" + filename);
-        response.setHeader("Content-Filename", filename);
 
         try {
             ServletOutputStream out = response.getOutputStream();
@@ -98,7 +107,7 @@ public class ExcelUtil {
             workbook.close();
             out.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new ApiException(e, ApiErrorCode.UNAUTHORIZED);
         }
 
     }
@@ -124,7 +133,9 @@ public class ExcelUtil {
                 .filter(field -> field.isAnnotationPresent(ExcelColumn.class))
                 .toList();
 
-        if(fieldList.isEmpty()) throw new IllegalArgumentException("[" + clazz.getName() + "] Can't find field with @ExcelColumn annotation");
+        if(fieldList.isEmpty()) throw new ApiException(HttpStatus.SERVICE_UNAVAILABLE, "[" + clazz.getName() + "] Can't find field with @ExcelColumn annotation");
+
+        for (Field field : fieldList) field.setAccessible(true);
 
         return fieldList;
     }
