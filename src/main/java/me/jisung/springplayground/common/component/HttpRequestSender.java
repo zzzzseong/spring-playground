1ea59004490530fcde86e0ba2c7444ebb76e5ba6
zzzzseong <jung48182@gmail.com>
2024-10-23 22:07:24 +0900

    refactor(component): HttpRequestSender post, get method 예외처리 및 코드 리펙터링

--- src/main/java/me/jisung/springplayground/common/component/HttpRequestSender.java
+++ src/main/java/me/jisung/springplayground/common/component/HttpRequestSender.java
@@ -1,20 +1,22 @@
 package me.jisung.springplayground.common.component;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import me.jisung.springplayground.common.exception.Api5xxErrorCode;
-import me.jisung.springplayground.common.exception.ApiException;
-import me.jisung.springplayground.common.util.JsonUtil;
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
-import reactor.core.publisher.Mono;
-
 import java.net.URI;
 import java.util.Objects;
 import java.util.function.Consumer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.jisung.springplayground.common.util.JsonUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
@@ -23,37 +25,43 @@ public class HttpRequestSender {
 
     private final WebClient webClient;
 
-    public final Consumer<HttpHeaders> emptyHeaders = httpHeaders -> {};
+    public static final String DEFAULT_BODY_VALUE = "{}";
+    public static final Consumer<HttpHeaders> emptyHeaders = httpHeaders -> {};
 
-    public String post(URI uri, Consumer<HttpHeaders> headers, Object body) {
-        String bodyValue = this.getBodyValue(body);
+    public String post(URI uri, Consumer<HttpHeaders> headers, Object body) throws HttpStatusCodeException {
+        String bodyValue = Objects.isNull(body) ? DEFAULT_BODY_VALUE : JsonUtil.toJson(body);
         log.info("[POST REQ] uri: {}, headers: {}, body: {}", uri, this.getHttpHeaders(headers), bodyValue);
 
         String response = webClient
-                .post()
-                .uri(uri)
-                .headers(headers)
-                .bodyValue(bodyValue)
-                .exchangeToMono(res -> res.bodyToMono(String.class))
-                .onErrorResume(err -> Mono.error(new ApiException(err, Api5xxErrorCode.SERVICE_UNAVAILABLE)))
-                .block();
+            .post()
+            .uri(uri)
+            .headers(headers)
+            .bodyValue(bodyValue)
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError, err -> Mono.error(new HttpClientErrorException(err.statusCode())))
+            .onStatus(HttpStatusCode::is5xxServerError, err -> Mono.error(new HttpServerErrorException(err.statusCode())))
+            .bodyToMono(String.class)
+            .block();
 
-        if(Objects.isNull(response)) throw new ApiException(Api5xxErrorCode.SERVICE_UNAVAILABLE);
+        if(Objects.isNull(response)) throw new HttpServerErrorException(HttpStatus.SERVICE_UNAVAILABLE);
         log.info("[POST RES] response: {}", response);
         return response;
     }
 
-    public String get(URI uri, Consumer<HttpHeaders> headers) {
+    public String get(URI uri, Consumer<HttpHeaders> headers) throws HttpStatusCodeException {
         log.info("[GET REQ] uri: {}, headers: {}", uri, this.getHttpHeaders(headers));
-        String response = webClient
-                .get()
-                .uri(uri)
-                .headers(headers)
-                .exchangeToMono(res -> res.bodyToMono(String.class))
-                .onErrorResume(err -> Mono.error(new ApiException(err, Api5xxErrorCode.SERVICE_UNAVAILABLE)))
-                .block();
 
-        if(Objects.isNull(response)) throw new ApiException(Api5xxErrorCode.SERVICE_UNAVAILABLE);
+        String response = webClient
+            .get()
+            .uri(uri)
+            .headers(headers)
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError, err -> Mono.error(new HttpClientErrorException(err.statusCode())))
+            .onStatus(HttpStatusCode::is5xxServerError, err -> Mono.error(new HttpServerErrorException(err.statusCode())))
+            .bodyToMono(String.class)
+            .block();
+
+        if(Objects.isNull(response)) throw new HttpServerErrorException(HttpStatus.SERVICE_UNAVAILABLE);
         log.info("[GET RES] response: {}", response);
         return response;
     }
@@ -87,17 +95,5 @@ public class HttpRequestSender {
         HttpHeaders httpHeaders = new HttpHeaders();
         headers.accept(httpHeaders);
         return httpHeaders;
-    }
-
-    /**
-     * body Object 를 String 으로 변환하는 메서드
-     * @param body 변환 대상 Object
-     * @return json string converted from body object
-     * @throws ApiException INVALID_JSON_FORMAT_5XX: if the body is not valid json format
-     * */
-    private String getBodyValue(Object body) {
-        String bodyValue = JsonUtil.toJson(body);
-        JsonUtil.validateJsonFormat(bodyValue);
-        return bodyValue;
     }
 }
