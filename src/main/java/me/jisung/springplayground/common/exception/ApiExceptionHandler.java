f463d0d75a3685dd556e693e0edcd964bd1c4765
zzzzseong <jung48182@gmail.com>
2024-10-26 15:07:11 +0900

    feat(exception): ExceptionHandler add handler for request param validation

--- src/main/java/me/jisung/springplayground/common/exception/ApiExceptionHandler.java
+++ src/main/java/me/jisung/springplayground/common/exception/ApiExceptionHandler.java
@@ -1,14 +1,14 @@
 package me.jisung.springplayground.common.exception;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import static me.jisung.springplayground.common.util.ApiResponseUtil.fail;
 
 import java.net.BindException;
 import java.util.Objects;
-
-import static me.jisung.springplayground.common.util.ApiResponseUtil.fail;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j(topic = "ApiExceptionHandler")
@@ -16,7 +16,7 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
     public String apiExceptionHandler(ApiException e) {
-        log.error("[API RESPONSE FAIL] ApiException - code: {}, message: {}", e.getCode(), e.getMessage());
+        log.error("[API RESPONSE FAILED] ApiException - code: {}, message: {}", e.getCode(), e.getMessage());
 
         Throwable cause = e.getCause();
         if(!Objects.isNull(cause)) log.error("caused by: ", cause);
@@ -24,15 +24,49 @@ public class ApiExceptionHandler {
         return fail(e.getCode(), e.getMessage());
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
-    public String validationExceptionHandler(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        ApiException apiException = new ApiException(Api4xxErrorCode.INVALID_REQUEST_BODY, message);
+    @ExceptionHandler(Exception.class)
+    public String unhandledExceptionHandler(Exception e) {
+        Api5xxErrorCode errorCode = Api5xxErrorCode.UNHANDLED_EXCEPTION;
+        ApiException apiException = ApiException.builder()
+            .e(e)
+            .httpStatus(errorCode.getHttpStatus())
+            .code(errorCode.getCode())
+            .message(e.getMessage())
+            .build();
         return apiExceptionHandler(apiException);
     }
 
-    @ExceptionHandler(Exception.class)
-    public String unhandledExceptionHandler(Exception e) {
-        return apiExceptionHandler(new ApiException(e, Api5xxErrorCode.UNHANDLED_EXCEPTION));
+    /**
+     * RequestParam validation exception handler
+     * */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String validationExceptionHandler(MissingServletRequestParameterException e) {
+        Api4xxErrorCode errorCode = Api4xxErrorCode.INVALID_REQUEST_PARAMETER;
+        ApiException apiException = ApiException.builder()
+            .e(e)
+            .httpStatus(errorCode.getHttpStatus())
+            .code(errorCode.getCode())
+            .message(e.getMessage())
+            .build();
+
+        return apiExceptionHandler(apiException);
+    }
+
+    /**
+     * RequestBody, ModelAttribute validation exception handler
+     * */
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    public String validationExceptionHandler(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        Api4xxErrorCode errorCode = Api4xxErrorCode.INVALID_REQUEST_BODY;
+        ApiException apiException = ApiException.builder()
+            .e(e)
+            .httpStatus(errorCode.getHttpStatus())
+            .code(errorCode.getCode())
+            .message(message)
+            .build();
+
+        return apiExceptionHandler(apiException);
     }
 }
