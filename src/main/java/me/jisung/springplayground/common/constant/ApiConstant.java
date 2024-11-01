855ab683cff8d04b1ad605e02ff3ee3734ebeb62
zzzzseong <jung48182@gmail.com>
2024-11-01 13:23:02 +0900

    feat(json): page request 상속용 dto 추가

--- src/main/java/me/jisung/springplayground/common/constant/ApiConstant.java
+++ src/main/java/me/jisung/springplayground/common/constant/ApiConstant.java
@@ -9,4 +9,9 @@ public class ApiConstant {
 
     /* validation */
     public static final String NOT_NULL_MESSAGE = "필수 입력값이 누락되었습니다.";
+
+    public static final int MIN_PAGE_NUMBER = 1;
+    public static final int MAX_PAGE_NUMBER = 100;
+    public static final int MIN_PAGE_SIZE = 1;
+    public static final int MAX_PAGE_SIZE = 1000;
 }
