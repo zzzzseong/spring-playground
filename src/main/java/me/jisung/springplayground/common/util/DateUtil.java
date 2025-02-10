3c7bb832c9e0a930055cd1c93a6c25497592f51a
zzzzseong <jung48182@gmail.com>
2025-02-10 13:42:51 +0900

    feat(common): DateUtil 함수 추가

--- src/main/java/me/jisung/springplayground/common/util/DateUtil.java
+++ src/main/java/me/jisung/springplayground/common/util/DateUtil.java
@@ -1,10 +1,11 @@
 package me.jisung.springplayground.common.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DateUtil {
@@ -17,10 +18,25 @@ public class DateUtil {
     public static ZonedDateTime now() {
         return ZonedDateTime.now(ZoneId.of(DateUtil.DATE_TIME_ZONE));
     }
-
     public static String now(String format) {
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of(DateUtil.DATE_TIME_ZONE));
         return now.format(DateTimeFormatter.ofPattern(format));
     }
 
+    public static String format(ZonedDateTime datetime) {
+        return datetime.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
+    }
+    public static String format(ZonedDateTime datetime, String format) {
+        return datetime.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static ZonedDateTime startOfDay(ZonedDateTime datetime) {
+        return datetime.withHour(0).withMinute(0).withSecond(0);
+    }
+    public static ZonedDateTime endOfDay(ZonedDateTime datetime) {
+        return datetime.withHour(23).withMinute(59).withSecond(59);
+    }
+    public static int lastDayOfMonth(ZonedDateTime datetime) {
+        return datetime.toLocalDate().lengthOfMonth();
+    }
 }
