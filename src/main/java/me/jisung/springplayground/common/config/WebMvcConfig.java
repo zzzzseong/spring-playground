af327e25c25e5494bf81d3ffd0ce3cef1538d505
zzzzseong <jung48182@gmail.com>
2025-06-04 16:10:39 +0900

    feat(logback): 로그 분리 로직 추가

--- src/main/java/me/jisung/springplayground/common/config/WebMvcConfig.java
+++ src/main/java/me/jisung/springplayground/common/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package me.jisung.springplayground.common.config;
 
-import me.jisung.springplayground.common.converter.CustomMessageConverter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -20,8 +21,47 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .allowCredentials(true);
     }
 
+    /**
+     * 사용자 정의 {@link HttpMessageConverter}를 등록합니다.
+     *
+     * <p>
+     * 이 메서드는 전달받은 {@code converters} 리스트에 사용자 정의 {@link HttpMessageConverter}를 추가합니다.
+     * 리스트의 인덱스 순서에 따라 {@code HttpMessageConverter}의 스캔 우선순위가 결정됩니다.
+     * </p>
+     *
+     * <p>
+     * Spring에서 기본적으로 등록하는 {@code HttpMessageConverter}의 순서는 다음과 같습니다:
+     * </p>
+     *
+     * <ol>
+     *     <li>ByteArrayHttpMessageConverter</li>
+     *     <li>StringHttpMessageConverter</li>
+     *     <li>StringHttpMessageConverter</li>
+     *     <li>ResourceHttpMessageConverter</li>
+     *     <li>ResourceRegionHttpMessageConverter</li>
+     *     <li>AllEncompassingFormHttpMessageConverter</li>
+     *     <li>MappingJackson2HttpMessageConverter</li>
+     *     <li>MappingJackson2HttpMessageConverter</li>
+     *     <li>Jaxb2RootElementHttpMessageConverter</li>
+     * </ol>
+     *
+     * <p>
+     * 이러한 기본 컨버터는 아래 클래스 및 메서드에서 확인할 수 있습니다:
+     * </p>
+     *
+     * <ul>
+     *     <li>{@link org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport#requestMappingHandlerAdapter}</li>
+     *     <li>{@link org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter#setMessageConverters}</li>
+     * </ul>
+     *
+     * @param converters Spring에 의해 기본적으로 등록된 {@code HttpMessageConverter} 목록
+     */
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(0, new CustomMessageConverter());
+        // 가장 앞에 추가
+//        converters.add(0, new CustomMessageConverter());
+
+        // 가장 뒤에 추가
+//        converters.add(new CustomMessageConverter());
     }
 }
--- src/main/java/me/jisung/springplayground/common/util/StringUtil.java
+++ src/main/java/me/jisung/springplayground/common/util/StringUtil.java
@@ -3,8 +3,6 @@ package me.jisung.springplayground.common.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StringUtil {
 
@@ -12,14 +10,14 @@ public class StringUtil {
 
 
     public static boolean isEmpty(String value) {
-        return Objects.isNull(value) || value.isEmpty() || Objects.equals(NULL, value);
+        return value == null || value.isEmpty() || NULL.equals(value);
     }
 
     public static String getOrDefault(String value, String defaultValue) {
         return isEmpty(value) ? defaultValue : value;
     }
 
-    public static String ifNull(String target, String defaultValue, String nonNullValue) {
-        return Objects.isNull(target) ? defaultValue : nonNullValue;
+    public static String ifEmpty(String target, String defaultValue, String nonNullValue) {
+        return isEmpty(target) ? defaultValue : nonNullValue;
     }
 }
