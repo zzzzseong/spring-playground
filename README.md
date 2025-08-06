e4d51d3bf78e3a4654facfa40f715d6304a49d9e
zzzzseong <jung48182@gmail.com>
2025-08-06 10:34:46 +0900

    feat(common): 프로퍼티 암호화를 위한 Jasypt 속성 추가

--- README.md
+++ README.md
@@ -19,21 +19,30 @@ spring.application.name=spring-playground
 # docker mysql
 spring.jpa.hibernate.ddl-auto=validate
 spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
-spring.datasource.url=should_override
-spring.datasource.username=should_override
-spring.datasource.password=should_override
+spring.datasource.url=override
+spring.datasource.username=override
+spring.datasource.password=override
 ```
 
 application-local.properties
 ```properties
 spring.datasource.url=jdbc:mysql://localhost:3306/spring_playground
-spring.datasource.username=root
-spring.datasource.password=1234
+spring.datasource.username=playground
+spring.datasource.password=playground
 ```
 
 # Build
 ```bash
 ./gradlew clean build ${options}
 ```
-- -Dorg.gradle.java.home=${java_home}:  특정 JDK 버전으로 빌드
+- -Dorg.gradle.java.home=${java_home}: 특정 JDK 버전으로 빌드
 - -Dspring.profiles.active=${profile}: 특정 프로파일로 빌드
+
+# Run
+```bash
+# foreground
+java -jar playground.jar --spring.profiles.active=${profile} --jasypt.encryptor.password=${password}
+
+# background
+nohup java -jar playground.jar --spring.profiles.active=${profile} --jasypt.encryptor.password=${password} > /dev/null 2>&1 &
+```
---
+++ src/main/java/me/jisung/springplayground/common/config/JasyptConfig.java
@@ -0,0 +1,33 @@
+package me.jisung.springplayground.common.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptorPassword;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptorPassword);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize(1);
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
