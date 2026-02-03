a5056ce1543db263826d26c4c61eacbe66cdc860
zzzzseong <jung48182@gmail.com>
2026-02-03 16:43:03 +0900

    feat: Random 난수 생성 함수 추가

--- src/main/java/me/jisung/springplayground/common/util/NumberUtil.java
+++ src/main/java/me/jisung/springplayground/common/util/NumberUtil.java
@@ -4,13 +4,19 @@ import lombok.NoArgsConstructor;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.Random;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class NumberUtil {
 
     private static final NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+    private static final Random random = new Random();
 
     public static String format(int number) {
         return numberFormat.format(number);
+    }
+
+    public static int generateRandomNumber(int min, int max) {
+        return random.nextInt((max - min) + 1) + min;
     }
 }
